da7dcf59a3bb75296511e9e97dc42429d4337490
ridwanr00 <rasyid.r00@gmail.com>
2024-07-31 09:12:49 +0700

    final_project

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,7 +3,7 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
-const axios = require("axios")
+const axios = require("axios");
 
 public_users.post("/register", async (req, res) => {
   //Write your code here
@@ -37,10 +37,12 @@ public_users.post("/register", async (req, res) => {
 public_users.get("/", async (req, res) => {
   try {
     // Example of fetching data from an external source using Axios
-    const response = await axios.get("http://localhost:3000/*");
+    const response = await axios.get(
+      "https://ridwanr00.github.io/expressBookReviews/db.json"
+    );
     const books = response.data;
 
-    return res.status(200).json({ message: "Success", data: books});
+    return res.status(200).json({ message: "Success", data: books });
   } catch (error) {
     return res
       .status(500)
@@ -49,53 +51,144 @@ public_users.get("/", async (req, res) => {
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
-  //Write your code here
+// public_users.get("/isbn/:isbn", function (req, res) {
+//   //Write your code here
+//   const isbn = req.params.isbn;
+//   if (books[isbn]) {
+//     return res.status(200).json({ message: "Success", data: books[isbn] });
+//   } else {
+//     return res
+//       .status(404)
+//       .json({ message: `Book with ISBN ${isbn} is not found.` });
+//   }
+// });
+//Task 11
+
+public_users.get("/isbn/:isbn", async (req, res) => {
   const isbn = req.params.isbn;
-  if (books[isbn]) {
-    return res.status(200).json({ message: "Success", data: books[isbn] });
-  } else {
+
+  try {
+    // Example of fetching book data using Axios
+    const response = await axios.get(
+      `https://ridwanr00.github.io/expressBookReviews/db.json`
+    );
+    const book = response.data;
+
+    if (book) {
+      return res.status(200).json({ message: "Success", data: book[isbn] });
+    } else {
+      return res
+        .status(404)
+        .json({ message: `Book with ISBN ${isbn} is not found.` });
+    }
+  } catch (error) {
     return res
-      .status(404)
-      .json({ message: `Book with ISBN ${isbn} is not found.` });
+      .status(500)
+      .json({ message: "Error fetching book data", error: error.message });
   }
 });
 
 // Get book details based on author
-public_users.get("/author/:author", function (req, res) {
-  //Write your code here
+// public_users.get("/author/:author", function (req, res) {
+//   //Write your code here
+//   const author = req.params.author;
+//   let booksList = [];
+//   for (let x in books) {
+//     if (books[x].author === author) {
+//       booksList.push(books[x]);
+//     }
+//   }
+//   if (booksList.length > 0) {
+//     return res.status(200).json({ message: "Success", data: booksList });
+//   } else {
+//     return res
+//       .status(404)
+//       .json({ message: `Book with author ${author} is not found.` });
+//   }
+// });
+// Task 11
+public_users.get("/author/:author", async (req, res) => {
   const author = req.params.author;
-  let booksList = [];
-  for (let x in books) {
-    if (books[x].author === author) {
-      booksList.push(books[x]);
+
+  try {
+    const response = await axios.get(
+      `https://ridwanr00.github.io/expressBookReviews/db.json`
+    );
+    const books = response.data;
+
+    // Assuming books is an in-memory object or an array
+    let booksList = [];
+
+    // Simulating an asynchronous operation, e.g., fetching from a database
+    for (let x in books) {
+      if (books[x].author === author) {
+        booksList.push(books[x]);
+      }
     }
-  }
-  if (booksList.length > 0) {
-    return res.status(200).json({ message: "Success", data: booksList });
-  } else {
+
+    if (booksList.length > 0) {
+      return res.status(200).json({ message: "Success", data: booksList });
+    } else {
+      return res
+        .status(404)
+        .json({ message: `Books by author ${author} not found.` });
+    }
+  } catch (error) {
     return res
-      .status(404)
-      .json({ message: `Book with author ${author} is not found.` });
+      .status(500)
+      .json({ message: "Error fetching books data", error: error.message });
   }
 });
 
 // Get all books based on title
-public_users.get("/title/:title", function (req, res) {
-  //Write your code here
+// public_users.get("/title/:title", function (req, res) {
+//   //Write your code here
+//   const title = req.params.title;
+//   let booksList = [];
+//   for (let x in books) {
+//     if (books[x].title === title) {
+//       booksList.push(books[x]);
+//     }
+//   }
+//   if (booksList.length > 0) {
+//     return res.status(200).json({ message: "Success", data: booksList });
+//   } else {
+//     return res
+//       .status(404)
+//       .json({ message: `Book with title ${title} is not found.` });
+//   }
+// });
+// Task 12
+public_users.get("/title/:title", async (req, res) => {
   const title = req.params.title;
-  let booksList = [];
-  for (let x in books) {
-    if (books[x].title === title) {
-      booksList.push(books[x]);
+
+  try {
+    const response = await axios.get(
+      `https://ridwanr00.github.io/expressBookReviews/db.json`
+    );
+    const books = response.data;
+
+    // Assuming books is an in-memory object or an array
+    let booksList = [];
+
+    // Simulating an asynchronous operation, e.g., fetching from a database
+    for (let x in books) {
+      if (books[x].title === title) {
+        booksList.push(books[x]);
+      }
     }
-  }
-  if (booksList.length > 0) {
-    return res.status(200).json({ message: "Success", data: booksList });
-  } else {
+
+    if (booksList.length > 0) {
+      return res.status(200).json({ message: "Success", data: booksList });
+    } else {
+      return res
+        .status(404)
+        .json({ message: `Books by author ${title} not found.` });
+    }
+  } catch (error) {
     return res
-      .status(404)
-      .json({ message: `Book with title ${title} is not found.` });
+      .status(500)
+      .json({ message: "Error fetching books data", error: error.message });
   }
 });
 
